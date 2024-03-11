92349bbc7c5e1f1799e946f198372407241970c9
Uglyprincess <112811446+Uglypr1nces@users.noreply.github.com>
2024-03-11 18:20:10 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
 $MyDir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
 $SourceFolder = Join-Path -Path $MyDir -ChildPath "images"
-$TargetFolder = Join-Path -Path $MyDir -ChildPath "Online_TTT\Online_TTT\bin\Debug"
+$TargetFolder = Join-Path -Path $MyDir -ChildPath "Online_TTT\bin\Debug"
 
 Get-ChildItem -Path $SourceFolder -Filter '*.jpg' -File |
     ForEach-Object {
