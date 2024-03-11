b20119026cef89b415f742a728b91b5ccd622cb9
Uglyprincess <112811446+Uglypr1nces@users.noreply.github.com>
2024-03-11 19:17:19 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,6 +2,6 @@ $MyDir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
 $SourceFolder = Join-Path -Path $MyDir -ChildPath "images"
 $TargetFolder = Join-Path -Path $MyDir -ChildPath "Online_TTT\bin\Debug"
 
-Copy-Item -Path $SourceFolder -Destination $TargetFolder
+Copy-Item -Path $SourceFolder -Destination $TargetFolder -Recurse
 
 
